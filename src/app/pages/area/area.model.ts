6c70948f8e7f6d49514f59ba8609f5f7a72b10dd
emmanuel <e_mmanuel_7@hotmail.com>
2021-04-27 12:13:28 -0500

    area model y service

---
+++ src/app/pages/area/area.model.ts
@@ -0,0 +1,9 @@
+export class Area {
+  constructor (
+    public id?: number,
+    public activo?: boolean,
+    public fechaActualiza?: Date,
+    public fechaAlta?: Date,
+    public nombre?: string
+  ){}
+}
