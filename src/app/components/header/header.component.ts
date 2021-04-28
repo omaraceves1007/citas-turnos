ac0ad51fd8fc3f13dddc71d8827046e8b4cc77ae
emmanuel <e_mmanuel_7@hotmail.com>
2021-04-28 18:28:03 -0500

    ajsutes varios

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import * as moment from 'moment';
+moment.locale('es')
 
 @Component({
   selector: 'app-header',
@@ -7,9 +9,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  date: string = '';
+
   constructor() { }
 
   ngOnInit(): void {
+    this.date = moment().format('LLL');
   }
 
 }
